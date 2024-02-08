104455f484e938e00d5a1d58b9341d4debe46af8
telinv <linatorgashova2002@mail.ru>
2024-02-08 20:52:14 +0300

    week 21 tasks

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
+import { Chart } from "react-google-charts";
 import './App.css';
 
+export const data = [
+  ["Task", "Hours per Day"],
+  ["Work", 8],
+  ["Eat", 2],
+  ["Commute", 4],
+  ["Watch YouTube", 1],
+  ["Sleep", 7],
+  ["Sport", 2],
+];
+export const options = {
+  title: "My Daily Activities",
+};
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Chart
+  chartType="PieChart"
+  data={data}
+  options={options}
+  width="100%"
+  height="400px"
+  legendToggle
+/>
     </div>
   );
 }
